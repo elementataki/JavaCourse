7044eda5331b0483087b8ba1952fdb93c5b0049b
mhorbachov <mhorbachov@playtika.com>
2019-02-26 21:23:57 +0200

    edit HW1 Task2

--- src/main/java/com/playtika/automation/HomeWork1/Task2.java
+++ src/main/java/com/playtika/automation/HomeWork1/Task2.java
@@ -20,22 +20,19 @@ public class Task2 {
     public static void main(String[] args) {
 
         System.out.print("Enter the 5-digits number you want to decompose: ");
-
         Scanner console = new Scanner(System.in);
-
         int n = console.nextInt();
 
         numberDecomposition(n);
-
     }
 
     private static void numberDecomposition(int n) {
 
         int d1 = n / 10000;
-        int d2 = (n - d1 * 10000) / 1000;
-        int d3 = (n - d1 * 10000 - d2 * 1000) / 100;
-        int d4 = (n - d1 * 10000 - d2 * 1000 - d3 * 100) / 10;
-        int d5 = n - d1 * 10000 - d2 * 1000 - d3 * 100 - d4 * 10;
+        int d2 = decompose2(n, d1) / 1000;
+        int d3 = decompose3(decompose2(n, d1), d2) / 100;
+        int d4 = decompose4(decompose3(decompose2(n, d1), d2), d3) / 10;
+        int d5 = decompose5(decompose4(decompose3(decompose2(n, d1), d2), d3), d4);
 
         System.out.printf("Decomposition of the %d number is the following:%n", n);
         System.out.println(d1);
@@ -43,5 +40,21 @@ public class Task2 {
         System.out.println(d3);
         System.out.println(d4);
         System.out.println(d5);
+    }
+
+    private static int decompose2(int x, int y) {
+        return x - y * 10000;
+    }
+
+    private static int decompose3(int x, int y) {
+        return x - y * 1000;
+    }
+
+    private static int decompose4(int x, int y) {
+        return x - y * 100;
+    }
+
+    private static int decompose5(int x, int y) {
+        return x - y * 10;
     }
 }
