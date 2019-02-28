b2506f47a6471bb7913e878c4a108ddf45c8cdb5
mhorbachov <mhorbachov@playtika.com>
2019-02-28 23:19:57 +0200

    edit HM2 Task1

--- src/main/java/com/playtika/automation/HomeWork2/Task1.java
+++ src/main/java/com/playtika/automation/HomeWork2/Task1.java
@@ -22,60 +22,51 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Choose the cell size: 3, 6 or 9");
-        Scanner cell = new Scanner(System.in);
-        int cellSize = cell.nextInt();
+        System.out.println("Enter the cell width which is a multiple of three");
+        Scanner enterValue = new Scanner(System.in);
+        int cellWidth = enterValue.nextInt();
 
-        //finding the length of solid line
+        final int CELLS_QUANTITY = 4;
+        final int QUANTITY_OF_SEPARATORS_BETWEEN_CELLS = 5;
+        final int MULTIPLIER_OF_ROWS_QUANTITY = 3;
 
-        int solidLine = 5 + 4 * cellSize;
+        int solidLineSize = CELLS_QUANTITY * cellWidth + QUANTITY_OF_SEPARATORS_BETWEEN_CELLS;
 
-        //printing of the first line
+        if (cellWidth % 3 == 0) {
 
-        for (int i = 1; i <= solidLine; i++) {
+            printSolidLine(solidLineSize);
+
+            for (int j = 1; j <= CELLS_QUANTITY; j++) {
+
+                for (int k = 1; k <= (cellWidth / MULTIPLIER_OF_ROWS_QUANTITY); k++) {
+
+                    printCellString(cellWidth, CELLS_QUANTITY);
+                }
+
+                printSolidLine(solidLineSize);
+            }
+        } else {
+            System.out.println("Invalid data. Please re-run the program and try again");
+        }
+    }
+
+    private static void printSolidLine(int solidLineSize) {
+        for (int i = 1; i <= solidLineSize; i++) {
             System.out.print("*");
         }
 
         System.out.println();
+    }
 
-        //loop for printing blocks in vertical direction
+    private static void printCellString(int cellWidth, int cellsQuantity) {
 
-        for (int j = 1; j <= 4; j++) {
+        for (int i = 1; i <= cellsQuantity; i++) {
 
-            //loop for printing the quantity of lines in each block
-
-            for (int k = 1; k <= (cellSize / 3); k++) {
-
-                //conditions for printing blocks in horizontal direction
-
-                if (cellSize == 3) {
-
-                    for (int i = 1; i <= 4; i++) {
-                        System.out.print("*   ");
-                    }
-                }
-                if (cellSize == 6) {
-
-                    for (int i = 1; i <= 4; i++) {
-                        System.out.print("*      ");
-                    }
-                }
-                if (cellSize == 9) {
-
-                    for (int i = 1; i <= 4; i++) {
-                        System.out.print("*         ");
-                    }
-                }
-
-                System.out.println("*");
+            System.out.print("*");
+            for (int j = 1; j <= cellWidth; j++) {
+                System.out.print(" ");
             }
-
-            //solid line under the each block
-
-            for (int i = 1; i <= solidLine; i++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
+        System.out.println("*");
     }
 }
